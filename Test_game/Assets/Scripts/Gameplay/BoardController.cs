f7e181e816e523f27937e28fc33df1d919701794
Mikkel S <Mikkel123sej@gmail.com>
2022-07-14 18:18:37 +0200

    Moved board definition into a seperate file

--- Test_game/Assets/Scripts/Gameplay/BoardController.cs
+++ Test_game/Assets/Scripts/Gameplay/BoardController.cs
@@ -1,41 +1,11 @@
 using UnityEngine;
+using UnityEditor;
 
 namespace Gameplay
 {
-    public class BoardItem
-    {
-        public enum ItemType
-        {
-            Player,
-            Obstacle,
-            Enemy,
-        }
-
-        public ItemType Type;
-
-        public BoardItem(ItemType type = ItemType.Player)
-        {
-            Type = type;
-        }
-    }
-    
-    public class BoardField
-    {
-        public BoardItem Item;
-        
-        public BoardField(BoardItem item = null)
-        {
-            Item = item;
-        }
-    }
-
-    public class Board
-    {
-        
-    }
-    
     public class BoardController : MonoBehaviour
     {
         
+        [SerializeField] private Board board;
     }
 }
