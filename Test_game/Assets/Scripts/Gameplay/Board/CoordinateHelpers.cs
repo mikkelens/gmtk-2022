15676aa582d01b972f430061b722e08bb08876c8
Mikkel S <Mikkel123sej@gmail.com>
2022-07-14 20:50:47 +0200

    made coordinate placement way better

---
+++ Test_game/Assets/Scripts/Gameplay/Board/CoordinateHelpers.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// ReSharper disable MemberCanBePrivate.Global
+namespace Gameplay.Board
+{
+    public static class CoordinateHelpers
+    {
+        public static Vector3 CoordToPosition(this Vector2Int coordinates)
+        {
+            Vector2 verticalPos = coordinates * Board.Instance.TileScale;
+            return verticalPos.VerticalToHorizontalPosition();
+        }
+        
+        // "Vertical" position is on the Z plane in transform worldspace.
+        public static Vector3 VerticalToHorizontalPosition(this Vector2 pos, float height = 0f)
+        {
+            return new Vector3(pos.x, height, pos.y);
+        }
+        // "Horizontal" position is on the Y plane in the transform worldspace.
+        public static Vector2 HorizontalToVerticalPosition(this Vector3 pos)
+        {
+            return new Vector2(pos.x, pos.z);
+        }
+    }
+}
--- Test_game/Assets/Scripts/Gameplay/Board/Field.cs
+++ Test_game/Assets/Scripts/Gameplay/Board/Field.cs
@@ -14,10 +14,22 @@ namespace Gameplay.Board
     public class Field : MonoBehaviour
     {
         // own details
-        public FieldType type;
+        private Vector2Int _coordinates;
+        private FieldType _type;
         
+        [ShowInInspector]
+        public Vector2Int Coordinates => _coordinates;
+        [ShowInInspector]
+        public FieldType Type => _type;
+
         // exterior details
         [AssetsOnly]
         public Item item;
+
+        public void SetCoordinates(Vector2Int coordinates)
+        {
+            _coordinates = coordinates;
+            transform.position = _coordinates.CoordToPosition();
+        }
     }
 }
